4da1d167d91e900eac1639646c71c235cac4dbcf
Hadi <hadi.yabari.m@gmail.com>
2026-02-14 12:01:43 +0330

    datebase
    its core base

---
+++ SQLQuery1.sql
@@ -0,0 +1,38 @@
+﻿USE [labsanalytics_db]
+GO
+
+EXEC sys.sp_dropextendedproperty @name=N'MS_Description' , @level0type=N'SCHEMA',@level0name=N'guest', @level1type=N'TABLE',@level1name=N'Security'
+GO
+
+/****** Object:  Table [guest].[Security]    Script Date: 1/8/2026 4:42:20 PM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[guest].[Security]') AND type in (N'U'))
+DROP TABLE [guest].[Security]
+GO
+
+/****** Object:  Table [guest].[Security]    Script Date: 1/8/2026 4:42:20 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [Security].[Security](
+	[user_id] [int] IDENTITY(1,1) NOT NULL,
+	[username] [nvarchar](30) NOT NULL,
+	[full_name] [nvarchar](50) NULL,
+	[depatment] [nvarchar](40) NULL,
+ CONSTRAINT [PK_Security] PRIMARY KEY CLUSTERED 
+(
+	[user_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [UQ_Security_username] UNIQUE NONCLUSTERED 
+(
+	[username] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'refrence table' , @level0type=N'SCHEMA',@level0name=N'guest', @level1type=N'TABLE',@level1name=N'Security'
+GO
+
+
